e040273c7a5e540c9c2475bd7962482a088a16de
ron <ron@olapic.com>
2016-04-06 11:20:49 -0400

    new message

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( slider.css )
+Rails.application.config.assets.precompile += %w( jquery-1.10.1.min.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery.sldr.js )
+Rails.application.config.assets.precompile += %w( slider.js )
